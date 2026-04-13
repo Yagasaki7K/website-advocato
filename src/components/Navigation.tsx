1c3b61f50333fe5513fb3915e1e8cb0470f737b0
Anderson Marlon <yagasakiwanderlust@proton.me>
2026-04-13 17:56:59 -0300

    Added: Components

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,16 @@
 import styled from "styled-components";
 
 const NavigationDetails = styled.div`
-
-`
+    h1 {
+        color: red;
+    }
+`;
 
 function Navigation() {
     return (
         <>
             <NavigationDetails>
-            <h1>Hello</h1>
+                <h1>Hello</h1>
             </NavigationDetails>
         </>
     );
