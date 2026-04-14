9a1b4383038f784a2c2b60421d2323f8cfef607b
Lucas <lu.slopesss@gmail.com>
2026-04-14 16:32:20 -0300

    feature:implementação da api através do express

---
+++ src/api/api.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import "dotenv/config";
+import axios from "axios";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/processos-judiciais/:numero", async (req, res) => {
+  const { numero } = req.params;
+  const apiUrl = process.env.API_URL!.replace("NUMERO", numero);
+  const { data } = await axios.get(apiUrl);
+
+  res.send(data);
+});
+
+app.listen(3000, () => {
+  console.log("Server running, all checked");
+});
